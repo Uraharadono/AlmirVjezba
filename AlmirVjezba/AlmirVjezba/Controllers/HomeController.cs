0ff7a84b4689d4bee9fab67bfb7356f21afe6647
almirmerzic <kiselavoda>
2018-05-14 22:58:31 +0200

    Reference Added

--- AlmirVjezba/AlmirVjezba/Controllers/HomeController.cs
+++ AlmirVjezba/AlmirVjezba/Controllers/HomeController.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AlmirVjezbaContext1.DAL;
+
 
 namespace AlmirVjezba.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
-
+        MyContext Db = new MyContext();
 
         public ActionResult Index()
         {
