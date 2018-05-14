ce39db009948f1734a4b73a7ad39a29132462f0b
almirmerzic <kiselavoda>
2018-05-14 22:31:05 +0200

    Class Meter Added

--- AlmirVjezba/AlmirVjezbaContext1/Model/Meter.cs
+++ AlmirVjezba/AlmirVjezbaContext1/Model/Meter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AlmirVjezbaContext1.Helper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace AlmirVjezbaContext1.Model
 {
-    public class Meter:IEntity
+    public class Meter : IEntity
     {
+        public long Id { get; set; }
+        public bool IsDeleted { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfInstallation { get; set; }
     }
 }
