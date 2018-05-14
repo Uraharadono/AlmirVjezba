2160e12849ae585e387293b1135c1cf245826220
almirmerzic <kiselavoda>
2018-05-14 22:22:34 +0200

    Class Address Added

--- AlmirVjezba/AlmirVjezbaContext1/Model/Address.cs
+++ AlmirVjezba/AlmirVjezbaContext1/Model/Address.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AlmirVjezbaContext1.Helper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,11 @@ namespace AlmirVjezbaContext1.Model
 {
     public class Address:IEntity 
     {
-        long Id { get; set; }
-        bool IsDeleted { get; set; }
+        public long Id { get; set; }
+        public bool IsDeleted { get; set; }
+        public string Street { get; set; }
+        public int Number { get; set; }
+        public string Addition { get; set; }
+        public string ZipCode { get; set; }
     }
 }
