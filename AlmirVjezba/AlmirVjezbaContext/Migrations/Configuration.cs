8447cd55fbb286a0ac7a3ffa28133276cc6fb356
almirmerzic <kiselavoda>
2018-05-15 15:38:59 +0200

    Add migrations to project

---
+++ AlmirVjezba/AlmirVjezbaContext/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace AlmirVjezbaContext.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<AlmirVjezbaContext.Context.MyContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(AlmirVjezbaContext.Context.MyContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
